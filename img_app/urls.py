1018d91c22a378162382c9ded48c5d1160fd1d39
AlexanderVin <alexandr.vinitsky@gmail.com>
2021-07-13 22:22:22 +0300

    fix

--- img_app/urls.py
+++ img_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import index, resize_img, add_img
+from .views import index, resize_img, CreateImgView
 
 app_name = 'img_app'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('add_img/', add_img, name='add_img'),
+    path('add_img/', CreateImgView.as_view(), name='add_img'),
     path('resize_img/', resize_img, name='resize_img'),
 
 ]
