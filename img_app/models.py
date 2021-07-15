b854ac27330cb6f9e2d68f4f4bda2bb77e29d909
alexandr.vinitsky <alexandr.vinitsky@gmail.com>
2021-07-15 10:54:55 +0300

    commit

--- img_app/models.py
+++ img_app/models.py
@@ -18,20 +18,20 @@ class ImageModel(models.Model):
     # width_origin = models.PositiveIntegerField(verbose_name='Ширина')
     # height_origin = models.PositiveIntegerField(verbose_name='Высота')
 
-    # def get_remote_image(self):
-    #     if self.image_url and not self.image_file:
-    #         result = urllib.urlretrieve(self.image_url)
-    #         self.image_file.save(
-    #             os.path.basename(self.image_url),
-    #             File(open(result[0]))
-    #         )
-    #         self.save()
-    def save_2(self):
-        p = requests.get(self.image_url)
-        out = open('MEDIA_URL' + 'self.image_url' + '.jpg', "wb")
-        out.write(p.content)
-        out.close()
-        self.save()
+    def get_remote_image(self):
+        if self.image_url and not self.image_file:
+            result = urllib.urlretrieve(self.image_url)
+            self.image_file.save(
+                os.path.basename(self.image_url),
+                File(open(result[0]))
+            )
+            self.save()
+    # def save_2(self):
+    #     p = requests.get(self.image_url)
+    #     out = open('MEDIA_URL' + 'self.image_url' + '.jpg', "wb")
+    #     out.write(p.content)
+    #     out.close()
+    #     self.save()
 
     def __str__(self):
         return self.title
